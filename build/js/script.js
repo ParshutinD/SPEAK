306fcd98ce11ab4c6fd5d90c6d128ac6ecb89b70
parshutin <daniilparshutin1@gmail.com>
2022-03-29 19:27:36 +0300

    Сообщение

--- build/js/script.js
+++ build/js/script.js
@@ -5,6 +5,18 @@ const our_psychologists_slider = new Swiper(".our-psychologists__slider", {
     el: ".our-psychologists__pagination",
     clickable: true,
   },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+
+    1023: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
 });
 
 const topic = document.querySelectorAll(".our-psychologists__item");
@@ -19,7 +31,14 @@ const feedback_slider = new Swiper(".feedback-block__slider", {
   slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
-    el: ".our-psychologists__pagination",
+    el: ".feedback-block__pagination",
     clickable: true,
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
   },
 });
